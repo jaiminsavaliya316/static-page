fc902fd1692709dde7bc3240373ac7a063c2bb7b
Jaimin Savaliya <jaiminsavaliya316@gmail.com>
2025-01-31 18:17:48 -0500

    static page contant

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function Page() {
+    return (
+        <div>
+            <header>
+                <img src="src\react js_logo_react_react native_icon.svg" width="40px" alt="React logo" />
+            </header>
+            <main>
+                <h1>Reason I am excited to learn React</h1>
+                <ol>
+                    <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                    <li>I am more likely to get a job as a front end developer if I know React</li>
+                </ol>
+            </main>
+            <footer>
+                <small>© 2024 Ziroll development. All rights reserved.</small>
+            </footer>
+        </div>
+    )
+}
+
+root.render(
+    <Page />
 )
